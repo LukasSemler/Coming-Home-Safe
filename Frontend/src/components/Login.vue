a980740464a345639007c4b647572b00c1f027ce
LukasSemler <semler.l04@htlwienwest.com>
2022-01-27 10:18:34 +0100

    Login Register begonnen

---
+++ Frontend/src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+<v-container>
+  <h2 class="text-center">Login</h2>
+  <p @click="changePage">get Back to Register</p>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {};
+  },
+  methods: {
+    changePage(){
+      localStorage.removeItem('firstTime')
+      localStorage.setItem('firstTime', true);
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ Frontend/src/views/Login_Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <h1 class="text-center">Willkommen bei Coming Home Safe</h1>
+    <br />
+    <v-container v-if="firstTime">
+      <Register />
+    </v-container>
+
+    <v-container v-else>
+      <Login />
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+import Login from '../components/Login';
+import Register from '../components/Register';
+
+export default {
+  name: 'Login_Register',
+  data() {
+    return {
+      firstTime: true,
+    };
+  },
+  components: {
+    Login,
+    Register,
+  },
+  mounted() {
+    const getItem = localStorage.getItem('firstTime');
+    console.log(getItem);
+  },
+};
+</script>
