f3a0e92cc5ef17e577930191a45a3173a515ff22
LukasSemler <semler.l04@htlwienwest.at>
2022-01-29 16:48:03 +0100

    PWA begonnen

---
+++ Backend/public/precache-manifest.68e873d6aae160e5cb8f42782d09c1df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc16b862c2f32bfc59a5",
+    "url": "/css/app.042c9d40.css"
+  },
+  {
+    "revision": "00e52f9eb82916a67e72",
+    "url": "/css/chunk-vendors.a70f7879.css"
+  },
+  {
+    "revision": "47fdef32a8ed3c2c03af1164fa13316f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc16b862c2f32bfc59a5",
+    "url": "/js/app.8385006b.js"
+  },
+  {
+    "revision": "00e52f9eb82916a67e72",
+    "url": "/js/chunk-vendors.b682846d.js"
+  },
+  {
+    "revision": "6d6dc6d2fce97257f4538dec0fd7e24b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
