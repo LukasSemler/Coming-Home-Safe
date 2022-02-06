fb1ea85e834ab427c245daf53e0df225e5ebd473
LukasSemler <semler.l04@htlwienwest.at>
2022-02-06 20:06:06 +0100

    Admin map show user + set Focus

--- Frontend/src/components/Google_Maps.vue
+++ Frontend/src/components/Google_Maps.vue
@@ -1,145 +1,145 @@
 <template>
-  <v-container>
-    <v-container>
-      <h2 class="text-center">Track Position</h2>
-      <!-- <GmapAutocomplete @place_changed="setPlace" />
+	<v-container>
+		<v-container>
+			<h2 class="text-center">Track Position</h2>
+			<!-- <GmapAutocomplete @place_changed="setPlace" />
       <button @click="addMarker">Add</button> -->
-    </v-container>
-    <br />
-    <GmapMap :center="center" :zoom="12" style="width: 100%; height: 800px">
-      <GmapMarker :key="index" v-for="(m, index) in locations" :position="m" />
-    </GmapMap>
+		</v-container>
+		<br />
+		<GmapMap :center="center" :zoom="12" style="width: 100%; height: 800px">
+			<GmapMarker :key="index" v-for="(m, index) in locations" :position="m" />
+		</GmapMap>
 
-    <br />
-    <br />
+		<br />
+		<br />
 
-    <v-container>
-      <v-row class="justify-center">
-        <v-btn class="light-blue accent-3" @click="startTracker">{{ text }}</v-btn>
-      </v-row>
-    </v-container>
-    <ul v-for="(pos, i) in loc" :key="i">
-      <li>latitude: {{ pos.lat }} longitude: {{ pos.lng }} time: {{ pos.dateTime }}</li>
-    </ul>
-  </v-container>
+		<v-container>
+			<v-row class="justify-center">
+				<v-btn class="light-blue accent-3" @click="startTracker">{{ text }}</v-btn>
+			</v-row>
+		</v-container>
+		<ul v-for="(pos, i) in loc" :key="i">
+			<li>latitude: {{ pos.lat }} longitude: {{ pos.lng }} time: {{ pos.dateTime }}</li>
+		</ul>
+	</v-container>
 </template>
 
 <script>
 export default {
-  name: 'GoogleMap',
-  data() {
-    return {
-      center: {},
-      currentPlace: null,
-      markers: [],
-      locations: [],
-      loc: [],
+	name: 'GoogleMap',
+	data() {
+		return {
+			center: {},
+			currentPlace: null,
+			markers: [],
+			locations: [],
+			loc: [],
 
-      text: 'Start Tracker',
-      started: false,
-      interval: '',
+			text: 'Start Tracker',
+			started: false,
+			interval: '',
 
-      ws: null,
-      ws_serverAdress: 'ws://localhost:2410',
-    };
-  },
-  created() {
-    window.addEventListener('beforeunload', (e) => {
-      let confirmationMessage =
-        ('Wenn Sie die Webseite verlassen, während der Tracker läuft, wird dieser beendet'(
-          e || window.event,
-        ).returnValue = confirmationMessage); //Gecko + IE
-      return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
-    });
-  },
-  mounted() {
-    this.setLocationLatLng();
-    this.ws = new WebSocket(this.ws_serverAdress);
-    this.ws.onmessage = ({ data }) => {
-      console.log(data);
-      try {
-        let bekommen = JSON.parse(data);
-        console.log(bekommen);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  },
-  methods: {
-    backToMain() {
-      this.$router.push({ name: 'Login_Register' });
-    }, //
-    setLocationLatLng() {
-      navigator.geolocation.getCurrentPosition((geolocation) => {
-        let currPos = {
-          lat: geolocation.coords.latitude,
-          lng: geolocation.coords.longitude,
-        };
+			ws: null,
+			ws_serverAdress: 'ws://localhost:2410',
+		};
+	},
+	created() {
+		window.addEventListener('beforeunload', (e) => {
+			let confirmationMessage =
+				('Wenn Sie die Webseite verlassen, während der Tracker läuft, wird dieser beendet'(
+					e || window.event,
+				).returnValue = confirmationMessage); //Gecko + IE
+			return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+		});
+	},
+	mounted() {
+		this.setLocationLatLng();
+		this.ws = new WebSocket(this.ws_serverAdress);
+		this.ws.onmessage = ({ data }) => {
+			console.log(data);
+			try {
+				let bekommen = JSON.parse(data);
+				console.log(bekommen);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+	},
+	methods: {
+		backToMain() {
+			this.$router.push({ name: 'Login_Register' });
+		}, //
+		setLocationLatLng() {
+			navigator.geolocation.getCurrentPosition((geolocation) => {
+				let currPos = {
+					lat: geolocation.coords.latitude,
+					lng: geolocation.coords.longitude,
+				};
 
-        this.$store.state.currentPosition = currPos;
-        console.log(`Current position Google Maps = ${currPos}`);
-        this.center = currPos;
+				this.$store.state.currentPosition = currPos;
+				console.log(`Current position Google Maps = ${currPos}`);
+				this.center = currPos;
 
-        this.locations = [
-          {
-            lat: geolocation.coords.latitude,
-            lng: geolocation.coords.longitude,
-            lable: 'Current Position',
-          },
-        ];
-      });
-    },
+				this.locations = [
+					{
+						lat: geolocation.coords.latitude,
+						lng: geolocation.coords.longitude,
+						lable: 'Current Position',
+					},
+				];
+			});
+		},
 
-    startTracker() {
-      if (!this.started) {
-        this.started = true;
-        this.text = 'Stop Tracker';
-        this.interval = setInterval(this.track, 2000);
-      } else {
-        this.started = false;
-        this.text = 'Start Tracker';
-        clearInterval(this.interval);
-      }
-    },
-    async track() {
-      let lat = '';
-      let lng = '';
-      let position = '';
-      let date = new Date();
-      // console.log(this.locations);
+		startTracker() {
+			if (!this.started) {
+				this.started = true;
+				this.text = 'Stop Tracker';
+				this.interval = setInterval(this.track, 2000);
+			} else {
+				this.started = false;
+				this.text = 'Start Tracker';
+				clearInterval(this.interval);
+			}
+		},
+		async track() {
+			let lat = '';
+			let lng = '';
+			let position = '';
+			let date = new Date();
+			// console.log(this.locations);
 
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(async (pos) => {
-          lat = Number(pos.coords.latitude);
-          lng = Number(pos.coords.longitude);
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(async (pos) => {
+					lat = Number(pos.coords.latitude);
+					lng = Number(pos.coords.longitude);
 
-          //Object fuer DB bauen
-          position = {
-            lat,
-            lng,
-            dateTime: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
-            id: 1,
-            user: this.$store.state.aktiverUserTest,
-          };
+					//Object fuer DB bauen
+					position = {
+						lat,
+						lng,
+						dateTime: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+						id: 1,
+						user: this.$store.state.aktiverUser,
+					};
 
-          this.ws.send(JSON.stringify(position));
+					this.ws.send(JSON.stringify(position));
 
-          this.loc.push(position); //Marker auf aktuelle position setzten
-          this.locations = [
-            {
-              lat: pos.coords.latitude,
-              lng: pos.coords.longitude,
-              lable: 'Current Position',
-            },
-          ];
+					this.loc.push(position); //Marker auf aktuelle position setzten
+					this.locations = [
+						{
+							lat: pos.coords.latitude,
+							lng: pos.coords.longitude,
+							lable: 'Current Position',
+						},
+					];
 
-          //Axios Call Post
-          // const res = await axios.post(`${this.serverAdress}/location`, { position });
-        });
-      } else {
-        alert('Dieser Browser unterstützt die Abfrage der Geolocation nicht.');
-      }
-    },
-  },
+					//Axios Call Post
+					// const res = await axios.post(`${this.serverAdress}/location`, { position });
+				});
+			} else {
+				alert('Dieser Browser unterstützt die Abfrage der Geolocation nicht.');
+			}
+		},
+	},
 };
 </script>
--- Frontend/src/components/adminMap.vue
+++ Frontend/src/components/adminMap.vue
@@ -1,77 +1,115 @@
 <template>
-  <v-container>
-    <v-container>
-      <h2 class="text-center">Watch Position</h2>
-      <v-btn @click="backToMain">Back to Login/Register</v-btn>
-      <!-- <GmapAutocomplete @place_changed="setPlace" />
+	<v-container>
+		<v-container>
+			<h2 class="text-center">Watch Position</h2>
+			<v-btn @click="backToMain">Back to Login/Register</v-btn>
+			<!-- <GmapAutocomplete @place_changed="setPlace" />
       <button @click="addMarker">Add</button> -->
-    </v-container>
-    <br />
-    <GmapMap :center="center" :zoom="12" style="width: 100%; height: 800px">
-      <GmapMarker :key="index" v-for="(m, index) in locationSingleUser" :position="m" />
-    </GmapMap>
+		</v-container>
+		<br />
+		<GmapMap :center="center" :zoom="12" style="width: 100%; height: 800px">
+			<GmapMarker :key="index" v-for="(m, index) in locationSingleUser" :position="m" />
+		</GmapMap>
 
-    <br />
-    <h1 class="text-center" v-if="locationSingleUser.length == 0">
-      Es sind noch keine User vorhanden
-    </h1>
-    <h2 class="text-center" v-else>Aktiver User: {{ users.vorname }} {{ users.nachname }}</h2>
-    <br />
-    <br />
-  </v-container>
+		<br />
+		<h1 class="text-center" v-if="locationSingleUser.length == 0">
+			Es sind noch keine User vorhanden
+		</h1>
+		<!-- <h2 class="text-center" v-else>Aktiver User: {{ users.vorname }} {{ users.nachname }}</h2> -->
+
+		<v-container v-if="locationSingleUser.length != 0">
+			<v-card class="mx-auto" max-width="500" elevation="2">
+				<v-list-item three-line>
+					<v-list-item-content>
+						<v-list-item-title class="text-h5 mb-1"
+							>{{ users.vorname }} {{ users.nachname }}
+						</v-list-item-title>
+						<v-list-item-subtitle>
+							<b>Persoenliche Infos:</b> {{ users.hobbysinteressen }}</v-list-item-subtitle
+						>
+					</v-list-item-content>
+
+					<v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+				</v-list-item>
+
+				<v-card-actions>
+					<v-btn class="light-blue" outlined text @click="setFocus"> Show on Map </v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-container>
+		<v-btn class="light-blue" outlined text @click="setFocus2"> Show on Map2 </v-btn>
+		<br />
+		<br />
+	</v-container>
 </template>
 
 <script>
 export default {
-  name: 'GoogleMap',
-  data() {
-    return {
-      center: {
-        lat: 48.2094881,
-        lng: 16.3072147,
-      },
-      currentPlace: null,
-      markers: [],
-      locations: [],
-      loc: [],
+	name: 'GoogleMap',
+	data() {
+		return {
+			center: {
+				lat: 48.2094881,
+				lng: 16.3072147,
+			},
+			currentPlace: null,
+			markers: [],
+			locations: [],
+			loc: [],
 
-      text: 'Start Tracker',
-      started: false,
-      interval: '',
+			text: 'Start Tracker',
+			started: false,
+			interval: '',
 
-      ws: null,
-      ws_serverAdress: 'ws://localhost:2410',
-      locationSingleUser: [],
-      users: {},
-    };
-  },
-  created() {
-    this.ws = new WebSocket(this.ws_serverAdress);
-    this.ws.onmessage = ({ data }) => {
-      let bekommen = JSON.parse(data);
-      console.log(bekommen);
+			ws: null,
+			ws_serverAdress: 'ws://localhost:2410',
+			locationSingleUser: [],
+			users: {},
+			currentPos: null,
+		};
+	},
+	created() {
+		this.ws = new WebSocket(this.ws_serverAdress);
+		this.ws.onmessage = ({ data }) => {
+			let bekommen = JSON.parse(data);
+			this.currentPos = bekommen;
 
-      if (bekommen.type == 'info') {
-        this.locationSingleUser = [];
-      } else {
-        this.users = bekommen.user;
-        this.locationSingleUser = [
-          {
-            lat: bekommen.lat,
-            lng: bekommen.lng,
-            lable: 'Current Position User 1',
-          },
-        ];
-        this.$forceUpdate();
-      }
-    };
-  },
-  methods: {
-    backToMain() {
-      this.$router.push({
-        name: 'Login_Register',
-      });
-    }, //
-  },
+			if (bekommen.type == 'info') {
+				this.locationSingleUser = [];
+			} else {
+				this.users = bekommen.user;
+				this.locationSingleUser = [
+					{
+						lat: bekommen.lat,
+						lng: bekommen.lng,
+						lable: 'Current Position User 1',
+					},
+				];
+				//Center setzen
+				// this.center.lat = this.currentPos.lat;
+				// this.center.lng = this.currentPos.lng;
+				//this.$forceUpdate();
+			}
+		};
+	},
+	methods: {
+		backToMain() {
+			this.$router.push({
+				name: 'Login_Register',
+			});
+		},
+		setFocus() {
+			console.log('click');
+			console.log(this.center);
+			this.center = { lat: this.currentPos.lat, lng: this.currentPos.lng };
+			console.log(this.center);
+		},
+		setFocus2() {
+			console.log('click');
+			console.log(this.center);
+			this.center = { lat: 16.0, lng: 0.1234 };
+			console.log(this.center);
+		},
+	},
 };
 </script>
